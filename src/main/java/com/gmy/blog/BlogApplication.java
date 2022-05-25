0df010b2840f61244f9b8e5f6b95685af8436550
gmy <1508594767@qq.com>
2022-05-25 23:26:48 +0800

    环境搭建

---
+++ src/main/java/com/gmy/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.gmy.blog;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+@MapperScan("com.gmy.blog.dao")
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
