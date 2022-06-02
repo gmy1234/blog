59e52399abefe818f2337af2e5761028e6f9e8d0
gmy <1508594767@qq.com>
2022-06-03 00:17:59 +0800

    发送验证码接口， 自定义redis限流注解，操作日志注解

--- src/main/java/com/gmy/blog/BlogApplication.java
+++ src/main/java/com/gmy/blog/BlogApplication.java
@@ -3,6 +3,7 @@ package com.gmy.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 @MapperScan("com.gmy.blog.dao")
--- src/main/java/com/gmy/blog/api/UserAuthApi.java
+++ src/main/java/com/gmy/blog/api/UserAuthApi.java
@@ -1,6 +1,14 @@
 package com.gmy.blog.api;
 
+import com.gmy.blog.annotation.AccessLimit;
+import com.gmy.blog.service.UserAuthService;
+import com.gmy.blog.vo.Result;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author gmydl
@@ -10,5 +18,26 @@ import io.swagger.annotations.Api;
  * @date 2022/6/1 20:44
  */
 @Api("用户账号模块")
+@RestController
 public class UserAuthApi {
+
+    @Autowired
+    private UserAuthService userAuthService;
+
+    /**
+     * 发送邮箱验证码
+     *
+     * @param username 用户名
+     * @return {@link Result<>}
+     */
+    @AccessLimit(seconds = 60, maxCount = 1)
+    @ApiOperation(value = "发送邮箱验证码")
+    @ApiImplicitParam(name = "username", value = "用户名", required = true, dataType = "String")
+    @GetMapping("/user/code")
+    public Result<?> sendCode(String username) {
+        System.out.println(username);
+
+        return Result.ok();
+    }
+
 }
