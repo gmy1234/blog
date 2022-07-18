2ffee7764da4738ffd438e3ef6161aa8a6007bd6
gmy <1508594767@qq.com>
2022-07-18 10:07:57 +0800

    Minio 流上传

--- src/test/java/com/gmy/blog/MinioTest.java
+++ src/test/java/com/gmy/blog/MinioTest.java
@@ -4,13 +4,14 @@ import com.gmy.blog.constant.CommonConst;
 import io.minio.*;
 import io.minio.errors.*;
 import io.minio.http.Method;
+import io.minio.messages.Bucket;
 import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
+import java.io.*;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
@@ -55,6 +56,27 @@ public class MinioTest {
         }
     }
 
+
+    /**
+     * 添加文件路径
+     * @throws FileNotFoundException
+     */
+    @Test
+    public void uploadForStream() throws FileNotFoundException {
+        InputStream file = new FileInputStream("/Users/gmydl/Desktop/picture/new2.png");
+
+        try {
+            minioClient.putObject(PutObjectArgs.builder()
+                    .bucket(gmyBucketName)
+                    .object("dir/to/p.png")
+                    .stream(file, -1, 10485760)
+                    .build());
+        } catch (MinioException | InvalidKeyException | IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Test
     public void downLoad() {
         try {
@@ -76,11 +98,29 @@ public class MinioTest {
         // 获取图片的URL
         String URL = minioClient.getPresignedObjectUrl(
                 GetPresignedObjectUrlArgs.builder()
-                .method(Method.PUT)
+                .method(Method.GET)
                 .bucket(gmyBucketName)
                 .object("image1.png")
                 .expiry(1, TimeUnit.DAYS)
                 .build());
         System.out.println(URL);
+    }
+
+
+    /**
+     * 获取文件的访问路径
+     */
+    @Test
+    public void getInfo(){
+
+        try {
+            StatObjectResponse statObjectResponse = minioClient.statObject(StatObjectArgs.builder()
+                    .bucket(gmyBucketName)
+                    .object("p.png").build());
+            System.out.println(statObjectResponse);
+        } catch (MinioException | InvalidKeyException | IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
     }
 }
